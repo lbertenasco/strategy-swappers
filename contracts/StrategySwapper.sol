8605280381f471799f87ab06187044ecc375651d
b0dhidharma <b0dhidharma@protonmail.com>
2021-04-21 12:48:33 -0300

    feat: uniswap strategy swapper

--- contracts/StrategySwapper.sol
+++ contracts/StrategySwapper.sol
@@ -4,97 +4,21 @@ pragma solidity 0.8.3;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 interface IStrategySwapper {
-  struct Swap {
-    uint256 _id;
-    address _strategy;
-    address _tokenIn;
-    address _tokenOut;
-    uint256 _amount;
-    uint256 _maxSlippage;
-    uint256 _deadline;
-  }
-
-  function createSwap(
+  function swap(
     address _tokenIn,
     address _tokenOut,
-    uint256 _amount,
+    uint256 _amountIn,
     uint256 _maxSlippage,
     uint256 _deadline
-  ) external returns (uint256 _id);
+  ) external returns (uint256);
 
-  function createSwapAndWithdrawDebt(
+  function claim() external returns (uint256);
+
+  function swapAndClaim(
     address _tokenIn,
     address _tokenOut,
-    uint256 _amount,
+    uint256 _amountIn,
     uint256 _maxSlippage,
     uint256 _deadline
-  ) external returns (uint256, uint256);
-
-  function withdrawAllDebt() external;
-
-  function withdrawDebt(address _token) external returns (uint256);
-}
-
-abstract contract StrategySwapper is IStrategySwapper {
-  mapping(address => Swap[]) strategiesSwaps; // Strategy => Swaps[]
-  mapping(uint256 => Swap) swaps; // SwapId => Strategy
-  mapping(address => mapping(address => uint256)) tokenDebt; // Strategy => ERC20 => Debt
-  uint256 strategiesCounter = 0;
-
-  constructor() {}
-
-  function createSwap(
-    address _tokenIn,
-    address _tokenOut,
-    uint256 _amount,
-    uint256 _maxSlippage,
-    uint256 _deadline
-  ) external override returns (uint256 _id) {
-    _id = _createSwap(_tokenIn, _tokenOut, _amount, _maxSlippage, _deadline);
-  }
-
-  function _createSwap(
-    address _tokenIn,
-    address _tokenOut,
-    uint256 _amount,
-    uint256 _maxSlippage,
-    uint256 _deadline
-  ) internal virtual returns (uint256 _id) {
-    // requires
-    // take tokens from strategy
-    _id = strategiesCounter;
-    Swap memory _swap = Swap(_id, msg.sender, _tokenIn, _tokenOut, _amount, _maxSlippage, _deadline);
-    strategiesSwaps[msg.sender].push(_swap);
-    swaps[_id] = _swap;
-    strategiesCounter++;
-  }
-
-  function createSwapAndWithdrawDebt(
-    address _tokenIn,
-    address _tokenOut,
-    uint256 _amount,
-    uint256 _maxSlippage,
-    uint256 _deadline
-  ) external override returns (uint256 _id, uint256 _totalTokenDebt) {
-    _id = _createSwap(_tokenIn, _tokenOut, _amount, _maxSlippage, _deadline);
-    _totalTokenDebt = _withdrawDebt(_tokenOut);
-  }
-
-  function withdrawDebt(address _token) external override returns (uint256) {
-    // send all token debt
-    // set debt to 0
-  }
-
-  function _withdrawDebt(address _token) internal returns (uint256) {}
-
-  function withdrawAllDebt() external override {}
-
-  function _executeAndSaveSwap(uint256 _id) internal returns (uint256 _receivedFromSwap) {
-    // requires
-    Swap memory _swap = swaps[_id];
-    _receivedFromSwap = _executeSwap(_id);
-    tokenDebt[_swap._tokenOut][_swap._strategy] = tokenDebt[_swap._tokenOut][_swap._strategy] + _receivedFromSwap;
-  }
-
-  function _executeSwap(uint256 _id) internal virtual returns (uint256) {}
+  ) external returns (uint256);
 }
--- contracts/UniswapSwapper.sol
+++ contracts/UniswapSwapper.sol
@@ -2,47 +2,75 @@
 pragma solidity 0.8.3;
 
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import './StrategySwapper.sol';
 
 interface IUniswapSwapper is IStrategySwapper {}
 
-contract UniswapSwapper is IUniswapSwapper, StrategySwapper {
+contract UniswapSwapper is IUniswapSwapper {
+  using SafeERC20 for IERC20;
+
+  uint256 FEE_PRECISION = 10000;
   address uniswap;
 
   constructor(address _uniswap) {
     _uniswap = uniswap;
   }
 
-  function _createSwap(
-    address _tokenIn,
-    address _tokenOut,
-    uint256 _amount,
-    uint256 _maxSlippage,
-    uint256 _deadline
-  ) internal override returns (uint256 _id) {
-    _id = super._createSwap(_tokenIn, _tokenOut, _amount, _maxSlippage, _deadline);
-    super._executeAndSaveSwap(_id);
+  function _getMinAmountOut(
+    address[] memory _path,
+    uint256 _amountIn,
+    uint256 _maxSlippage
+  ) internal view returns (uint256 _minAmountOut) {
+    uint256 _amountOut = IUniswapV2Router02(uniswap).getAmountsOut(_amountIn, _path)[0];
+    _minAmountOut = _amountOut - ((_amountOut * _maxSlippage) / FEE_PRECISION / 100);
   }
 
-  function _executeSwap(uint256 _id) internal override returns (uint256 _totalReceived) {
-    address[] memory _path = new address[](2);
-    _path[0] = address(0); // from
-    _path[1] = address(0); // to
+  function swap(
+    address _tokenIn,
+    address _tokenOut,
+    uint256 _amountIn,
+    uint256 _maxSlippage,
+    uint256 _deadline
+  ) external override returns (uint256 _receivedAmount) {
+    _swap(msg.sender, _tokenIn, _tokenOut, _amountIn, _maxSlippage, _deadline);
+  }
 
-    // Approve given erc20
+  function _swap(
+    address _from,
+    address _tokenIn,
+    address _tokenOut,
+    uint256 _amountIn,
+    uint256 _maxSlippage,
+    uint256 _deadline
+  ) external override returns (uint256 _receivedAmount) {
+    IERC20(_tokenIn).safeTransferFrom(_from, address(this), _amountIn);
+
+    address[] memory _path = new address[](2);
+    _path[0] = _tokenIn;
+    _path[1] = _tokenOut;
+
     IERC20(_path[0]).safeApprove(uniswap, 0);
-    IERC20(_path[0]).safeApprove(
-      uniswap,
-      /* _amount */
-      0
-    );
-    // Swap it
-    IUniswapV2Router02(uniswap).swapExactTokensForTokens(
-      _amount,
-      0, // min amount (slippage calculation)
+    IERC20(_path[0]).safeApprove(uniswap, _amountIn);
+
+    _receivedAmount = IUniswapV2Router02(uniswap).swapExactTokensForTokens(
+      _amountIn,
+      _getMinAmountOut(_path, _amountIn, _maxSlippage),
       _path,
-      address(this),
-      now.add(1800)
-    );
+      _from,
+      _deadline
+    )[0];
+  }
+
+  function claim() external override returns (uint256) {}
+
+  function swapAndClaim(
+    address _tokenIn,
+    address _tokenOut,
+    uint256 _amountIn,
+    uint256 _maxSlippage,
+    uint256 _deadline
+  ) external override returns (uint256) {
+    _swap(msg.sender, _tokenIn, _tokenOut, _amountIn, _maxSlippage, _deadline);
   }
 }

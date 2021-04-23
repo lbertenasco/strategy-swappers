a05db6c811f40cb56a4526a6f5fc9a31e145d019
b0dhidharma <b0dhidharma@protonmail.com>
2021-04-23 15:30:00 -0300

    feat: yearn otc uniswap swapper

--- contracts/UniswapSwapper.sol
+++ contracts/UniswapSwapper.sol
@@ -23,6 +23,57 @@ contract UniswapSwapper is IUniswapSwapper, StrategySwapper {
     UNISWAP = _uniswap;
   }
 
+  function executeSwap(uint256 _id) external onlyMechanic isPendingSwap(_id) returns (uint256 _receivedAmount) {
+    Swap storage _swapInformation = _checkPreExecuteSwap(_id);
+
+    _receivedAmount = _executeUniswap(
+      _swapInformation.from,
+      _swapInformation.tokenIn,
+      _swapInformation.tokenOut,
+      _swapInformation.amountIn,
+      _swapInformation.maxSlippage
+    );
+
+    _deletePendingSwap(_swapInformation);
+  }
+
+  function _executeUniswap(
+    address _receiver,
+    address _tokenIn,
+    address _tokenOut,
+    uint256 _amountIn,
+    uint256 _maxSlippage
+  ) internal returns (uint256 _receivedAmount) {
+    address[] memory _path = _getPath(_tokenIn, _tokenOut);
+    uint256 _minAmountOut = _getMinAmountOut(_amountIn, _maxSlippage, _path);
+
+    if (_tokenIn == ETH) {
+      _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactETHForTokens{value: _amountIn}(
+        _minAmountOut,
+        _path,
+        _receiver,
+        block.timestamp + 1800
+      )[0];
+    } else {
+      IERC20(_path[0]).safeApprove(UNISWAP, 0);
+      IERC20(_path[0]).safeApprove(UNISWAP, _amountIn);
+
+      if (_tokenOut == ETH) {
+        _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactTokensForETH(_amountIn, _minAmountOut, _path, _receiver, block.timestamp + 1800)[
+          0
+        ];
+      } else {
+        _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactTokensForTokens(
+          _amountIn,
+          _minAmountOut,
+          _path,
+          _receiver,
+          block.timestamp + 1800
+        )[0];
+      }
+    }
+  }
+
   function _getMinAmountOut(
     uint256 _amountIn,
     uint256 _maxSlippage,
@@ -46,44 +97,5 @@ contract UniswapSwapper is IUniswapSwapper, StrategySwapper {
       _path[1] = WETH;
       _path[2] = _tokenOut;
     }
-  }
-
-  function executeSwap(uint256 _id) external onlyMechanic isPendingSwap(_id) returns (uint256 _receivedAmount) {
-    Swap storage _swapInformation = _checkPreExecuteSwap(_id);
-
-    address[] memory _path = _getPath(_swapInformation.tokenIn, _swapInformation.tokenOut);
-    uint256 _minAmountOut = _getMinAmountOut(_swapInformation.amountIn, _swapInformation.maxSlippage, _path);
-
-    if (_swapInformation.tokenIn == ETH) {
-      _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactETHForTokens{value: _swapInformation.amountIn}(
-        _minAmountOut,
-        _path,
-        _swapInformation.from,
-        _swapInformation.deadline
-      )[0];
-    } else {
-      IERC20(_path[0]).safeApprove(UNISWAP, 0);
-      IERC20(_path[0]).safeApprove(UNISWAP, _swapInformation.amountIn);
-
-      if (_swapInformation.tokenOut == ETH) {
-        _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactTokensForETH(
-          _swapInformation.amountIn,
-          _minAmountOut,
-          _path,
-          _swapInformation.from,
-          _swapInformation.deadline
-        )[0];
-      } else {
-        _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactTokensForTokens(
-          _swapInformation.amountIn,
-          _minAmountOut,
-          _path,
-          _swapInformation.from,
-          _swapInformation.deadline
-        )[0];
-      }
-    }
-
-    _deletePendingSwap(_swapInformation);
   }
 }
--- contracts/YearnOTCAndUniswapSwapper.sol
+++ contracts/YearnOTCAndUniswapSwapper.sol
@@ -3,37 +3,28 @@ pragma solidity 0.8.3;
 
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import './StrategySwapper.sol';
+import './UniswapSwapper.sol';
 
-interface IYearnOTCAndUniswapSwapper is IStrategySwapper {
-  function UNISWAP() external view returns (address);
-}
+interface IYearnOTCAndUniswapSwapper is IUniswapSwapper {}
 
-contract YearnOTCAndUniswapSwapper is IYearnOTCAndUniswapSwapper, StrategySwapper {
+// TODO: Adapt for ETH (in-out trades the OTC part)
+
+contract YearnOTCAndUniswapSwapper is IYearnOTCAndUniswapSwapper, UniswapSwapper {
   using SafeERC20 for IERC20;
-
-  address public immutable override UNISWAP;
 
   constructor(
     address _uniswap,
     address _mechanicsRegistry,
     address _weth,
     uint256 _slippagePrecision
-  ) StrategySwapper(_mechanicsRegistry, _weth, _slippagePrecision) {
-    UNISWAP = _uniswap;
-  }
+  ) UniswapSwapper(_uniswap, _mechanicsRegistry, _weth, _slippagePrecision) {}
 
-  function _getMinAmountOut(
+  function _getTotalAmountOut(
     uint256 _amountIn,
-    uint256 _maxSlippage,
-    address[] memory _path
-  ) internal view returns (uint256 _minAmountOut) {
-    uint256 _amountOut = _getTotalAmountOut(_amountIn, _path);
-    _minAmountOut = _amountOut - ((_amountOut * _maxSlippage) / SLIPPAGE_PRECISION / 100);
-  }
-
-  function _getTotalAmountOut(uint256 _amountIn, address[] memory _path) internal view returns (uint256 _amountOut) {
-    _amountOut = IUniswapV2Router02(UNISWAP).getAmountsOut(_amountIn, _path)[0];
+    address _tokenIn,
+    address _tokenOut
+  ) internal view returns (uint256 _amountOut) {
+    _amountOut = IUniswapV2Router02(UNISWAP).getAmountsOut(_amountIn, _getPath(_tokenIn, _tokenOut))[0];
   }
 
   function executeSwap(
@@ -44,27 +35,24 @@ contract YearnOTCAndUniswapSwapper is IYearnOTCAndUniswapSwapper, StrategySwappe
     Swap storage _swapInformation = _checkPreExecuteSwap(_id);
 
     // Take in from swapper
-    uint256 _totalOutNeeded = _getTotalAmountOut(_swapInformation.amountIn, _path);
-    IERC20(_swapInformation.tokenIn).safeTransferFrom(msg.sender, address(this), _providedAmount);
+    uint256 _totalOutNeeded = _getTotalAmountOut(_swapInformation.amountIn, _swapInformation.tokenIn, _swapInformation.tokenOut);
+    IERC20(_swapInformation.tokenOut).safeTransferFrom(msg.sender, address(this), _providedAmount);
 
     // Send what should be sent to swapper
-    uint256 _rewardedIn = _getTotalAmountOut(_providedAmount, _path); // reverse path
+    uint256 _rewardedIn = _getTotalAmountOut(_providedAmount, _swapInformation.tokenOut, _swapInformation.tokenIn);
     IERC20(_swapInformation.tokenIn).safeTransfer(msg.sender, _rewardedIn);
 
     // Buy what's missing from uniswap
     if (_providedAmount < _totalOutNeeded) {
       uint256 _toBuyFromUniswap = _swapInformation.amountIn - _rewardedIn;
 
-      IERC20(_path[0]).safeApprove(UNISWAP, 0);
-      IERC20(_path[0]).safeApprove(UNISWAP, _toBuyFromUniswap);
-
-      _receivedAmount = IUniswapV2Router02(UNISWAP).swapExactTokensForTokens(
-        _toBuyFromUniswap,
-        _getMinAmountOut(_toBuyFromUniswap, _swapInformation.maxSlippage, _path),
-        _path,
+      _receivedAmount = _executeUniswap(
         _swapInformation.from,
-        _swapInformation.deadline
-      )[0];
+        _swapInformation.tokenIn,
+        _swapInformation.tokenOut,
+        _toBuyFromUniswap,
+        _swapInformation.maxSlippage
+      );
     }
 
     _deletePendingSwap(_swapInformation);

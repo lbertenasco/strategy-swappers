1c5e373f42f5e76fea2c119c2bbe16f33bc40ec9
b0dhidharma <b0dhidharma@protonmail.com>
2021-05-07 16:29:38 -0300

    feat: collectable and governable on otc pool

--- contracts/OTCPool/OTCPool.sol
+++ contracts/OTCPool/OTCPool.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import './OTCPoolDesk.sol';
+import '@lbertenasco/contract-utils/contracts/utils/CollectableDust.sol';
+import '@lbertenasco/contract-utils/contracts/utils/Governable.sol';
+
 import './OTCPoolTradeable.sol';
+import './OTCPoolDesk.sol';
 
 interface IOTCPool is IOTCPoolTradeable {}
 
-contract OTCPoolis is IOTCPool, OTCPoolDesk, OTCPoolTradeable {
-  constructor(address _OTCProvider, address _swapperRegistry) OTCPoolDesk(_OTCProvider) OTCPoolTradeable(_swapperRegistry) {}
+contract OTCPool is IOTCPool, OTCPoolDesk, OTCPoolTradeable, Governable, CollectableDust {
+  constructor(
+    address _governor,
+    address _OTCProvider,
+    address _swapperRegistry
+  ) Governable(_governor) OTCPoolDesk(_OTCProvider) OTCPoolTradeable(_swapperRegistry) {}
 
-  // TODO: Only governance
-  function setOTCProvider(address _OTCProvider) external override {
+  // OTC Pool Desk
+  function setOTCProvider(address _OTCProvider) external override onlyGovernor {
     _setOTCProvider(_OTCProvider);
-  }
-
-  // TODO: Only governance
-  function setSwapperRegistry(address _swapperRegistry) external override {
-    _setSwapperRegistry(_swapperRegistry);
   }
 
   function deposit(
@@ -36,6 +38,10 @@ contract OTCPoolis is IOTCPool, OTCPoolDesk, OTCPoolTradeable {
   }
 
   // OTC Pool Tradeable
+  function setSwapperRegistry(address _swapperRegistry) external override onlyGovernor {
+    _setSwapperRegistry(_swapperRegistry);
+  }
+
   function claim(address _token, uint256 _amountToClaim) external override onlyOTCProvider {
     _claim(msg.sender, _token, _amountToClaim);
   }
@@ -46,5 +52,23 @@ contract OTCPoolis is IOTCPool, OTCPoolDesk, OTCPoolTradeable {
     uint256 _maxOfferedAmount
   ) external override onlyRegisteredSwapper returns (uint256 _tookFromPool, uint256 _tookFromSwapper) {
     (_tookFromPool, _tookFromSwapper) = _performTradeOnSwapper(msg.sender, _offeredTokenToPool, _wantedTokenFromPool, _maxOfferedAmount);
+  }
+
+  // Governable
+  function setPendingGovernor(address _pendingGovernor) external override onlyGovernor {
+    _setPendingGovernor(_pendingGovernor);
+  }
+
+  function acceptGovernor() external override onlyPendingGovernor {
+    _acceptGovernor();
+  }
+
+  // Collectable dust
+  function sendDust(
+    address _to,
+    address _token,
+    uint256 _amount
+  ) external virtual override onlyGovernor {
+    _sendDust(_to, _token, _amount);
   }
 }

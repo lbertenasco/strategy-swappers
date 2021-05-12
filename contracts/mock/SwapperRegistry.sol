dafbd6f10f8508c569285d298485e235fd5addd3
b0dhidharma <b0dhidharma@protonmail.com>
2021-05-12 18:36:39 -0300

    test: implements wireframe tests of swapper registry

--- contracts/mock/SwapperRegistry.sol
+++ contracts/mock/SwapperRegistry.sol
@@ -1,11 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
+import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+
 import '../SwapperRegistry.sol';
 
 contract SwapperRegistryMock is SwapperRegistry {
-
+  using EnumerableSet for EnumerableSet.AddressSet;
+  
   constructor(address _governance) SwapperRegistry(_governance) {}
+
+  function addNameByAddress(address _address, string memory _name) external {
+    nameByAddress[_address] = _name;
+  }
+
+  function addSwapperByName(string memory _name, address _address) external {
+    swapperByName[_name] = _address;
+  }
+
+  function addInitializationByAddress(address _address, uint256 _initialization) external {
+    initializationByAddress[_address] = _initialization;
+  }
+
+  function setDeprecatedByAddress(address _address, bool _deprecated) external {
+    deprecatedByAddress[_address] = _deprecated;
+  } 
+
+  function addSwappersToSwappers(address[] memory __swappers) external {
+    for (uint256 i = 0; i < __swappers.length; i++) {
+      _swappers.add(__swappers[i]);
+    }
+  }
+
+  function removeSwappersOfSwappers(address[] memory __swappers) external {
+    for (uint256 i = 0; i < __swappers.length; i++) {
+      _swappers.remove(__swappers[i]);
+    }
+  }
 
   function addSwapperInternal(string memory _name, address _swapper) external {
     _addSwapper(_name, _swapper);

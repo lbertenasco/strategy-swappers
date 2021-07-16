4b739507c116ec85f0c88d497d4b05f36b3eb8d2
b0dhidharma <78811746+b0dhidharma@users.noreply.github.com>
2021-07-16 17:35:56 -0300

    test: starts with e2e wireframes (#20)
    * refactor: simplified otc pool tradeable
    
    * test: starts with e2e wireframes

---
+++ test/e2e/OTCPool/claim-profits.spec.ts
@@ -0,0 +1,9 @@
+import { contract, then, when } from '../../utils/bdd';
+
+contract('OTCPool', () => {
+  describe('claim profits', () => {
+    when('trade has been executed on otc pool', () => {
+      then('otc provider gets tokens');
+    });
+  });
+});

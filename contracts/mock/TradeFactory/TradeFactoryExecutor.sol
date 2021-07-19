15b5a192a4da441344ec5ebfbc69b16f499abc74
b0dhidharma <b0dhidharma@protonmail.com>
2021-07-19 16:03:40 -0300

    feat: deprecates only strategy for oz access control

--- contracts/mock/TradeFactory/TradeFactoryExecutor.sol
+++ contracts/mock/TradeFactory/TradeFactoryExecutor.sol
@@ -5,15 +5,13 @@ import '../../TradeFactory/TradeFactoryExecutor.sol';
 
 import './TradeFactoryPositionsHandler.sol';
 
-contract TradeFactoryExecutorMock is TradeFactoryPositionsHandler, TradeFactoryExecutor {
-  constructor(address _swapperRegistry, address _mechanicsRegistry) TradeFactoryPositionsHandler(_swapperRegistry) TradeFactoryExecutor(_mechanicsRegistry) {}
-
-  function setMechanicsRegistry(address _mechanicsRegistry) external override {
-    _setMechanicsRegistry(_mechanicsRegistry);
-  }
+contract TradeFactoryExecutorMock is Governable, TradeFactoryPositionsHandler, TradeFactoryExecutor {
+  constructor(address _governor, address _swapperRegistry, address _mechanicsRegistry) 
+    Governable(_governor) 
+    TradeFactoryPositionsHandler(_swapperRegistry) 
+    TradeFactoryExecutor(_mechanicsRegistry) {}
 
   function enableSwapperToken(address _swapper, address _token) external {
     _enableSwapperToken(_swapper, _token);
   }
-
 }

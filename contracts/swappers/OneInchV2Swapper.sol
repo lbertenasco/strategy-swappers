7c027659567e1dfcb24cb2a825ec05bb42f27c16
b0dhidharma <b0dhidharma@protonmail.com>
2021-07-23 14:36:42 -0300

    feat: execute trades script

--- contracts/swappers/OneInchV2Swapper.sol
+++ contracts/swappers/OneInchV2Swapper.sol
@@ -44,7 +44,7 @@ interface IOneInchExchange {
 }
 
 interface IOneInchSwapper is IOneInchCaller, ISwapper {
-  function ONE_INCH() external view returns (address);
+  function AGGREGATION_ROUTER_V3() external view returns (address);
 
   function parts() external view returns (uint256);
 
@@ -58,19 +58,19 @@ interface IOneInchSwapper is IOneInchCaller, ISwapper {
 contract OneInchV2Swapper is IOneInchSwapper, Swapper {
   using SafeERC20 for IERC20;
 
-  address public immutable override ONE_INCH;
+  address public immutable override AGGREGATION_ROUTER_V3;
   uint256 public override parts;
   uint256 public override flags;
 
   constructor(
     address _governor,
     address _tradeFactory,
-    address _oneInch,
+    address _aggregationRouter,
     uint256 _parts,
     uint256 _flags
   ) Swapper(_governor, _tradeFactory) {
     require(_parts > 0, 'Swapper: Parts should be non zero');
-    ONE_INCH = _oneInch;
+    AGGREGATION_ROUTER_V3 = _aggregationRouter;
     parts = _parts;
     flags = _flags;
   }
@@ -102,28 +102,29 @@ contract OneInchV2Swapper is IOneInchSwapper, Swapper {
     address _tokenIn,
     address _tokenOut,
     uint256 _amountIn,
-    uint256 _maxSlippage
+    uint256 _maxSlippage,
+    bytes calldata _data
   ) internal override returns (uint256 _receivedAmount) {
-    uint256 _minAmountOut = _getMinAmountOut(_tokenIn, _tokenOut, _amountIn, _maxSlippage);
-    CallDescription[] memory _callDescriptions = new CallDescription[](0);
-    bytes memory _permit;
-    IERC20(_tokenIn).safeApprove(ONE_INCH, 0);
-    IERC20(_tokenIn).safeApprove(ONE_INCH, _amountIn);
-    _receivedAmount = IOneInchExchange(ONE_INCH).swap(
-      IOneInchCaller(address(this)),
-      IOneInchExchange.SwapDescription({
-        srcToken: IERC20(_tokenIn),
-        dstToken: IERC20(_tokenOut),
-        srcReceiver: address(0),
-        dstReceiver: _receiver,
-        amount: _minAmountOut,
-        minReturnAmount: _minAmountOut,
-        guaranteedAmount: 0,
-        flags: 0,
-        referrer: address(0),
-        permit: _permit
-      }),
-      _callDescriptions
-    );
+    // uint256 _minAmountOut = _getMinAmountOut(_tokenIn, _tokenOut, _amountIn, _maxSlippage);
+    // CallDescription[] memory _callDescriptions = new CallDescription[](0);
+    // bytes memory _permit;
+    // IERC20(_tokenIn).safeApprove(ONE_INCH, 0);
+    // IERC20(_tokenIn).safeApprove(ONE_INCH, _amountIn);
+    // _receivedAmount = IOneInchExchange(ONE_INCH).swap(
+    //   IOneInchCaller(address(this)),
+    //   IOneInchExchange.SwapDescription({
+    //     srcToken: IERC20(_tokenIn),
+    //     dstToken: IERC20(_tokenOut),
+    //     srcReceiver: address(0),
+    //     dstReceiver: _receiver,
+    //     amount: _minAmountOut,
+    //     minReturnAmount: _minAmountOut,
+    //     guaranteedAmount: 0,
+    //     flags: 0,
+    //     referrer: address(0),
+    //     permit: _permit
+    //   }),
+    //   _callDescriptions
+    // );
   }
 }

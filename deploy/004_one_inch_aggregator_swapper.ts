149e6bbe909d9852630234a80b499b16a9e9c7b9
b0dhidharma <78811746+b0dhidharma@users.noreply.github.com>
2021-07-28 16:38:50 -0300

    feat: adds one inch aggregator swapper (#33)

---
+++ deploy/004_one_inch_aggregator_swapper.ts
@@ -0,0 +1,37 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { getChainId, shouldVerifyContract } from '../utils/deploy';
+
+const AGGREGATION_ROUTER_V3: { [chainId: string]: string } = {
+  // Mainnet
+  '1': '0x11111112542D85B3EF69AE05771c2dCCff4fAa26',
+  // Polygon
+  '137': '0x11111112542D85B3EF69AE05771c2dCCff4fAa26',
+};
+
+const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer, governor } = await hre.getNamedAccounts();
+
+  const tradeFactory = await hre.deployments.get('TradeFactory');
+
+  const chainId = await getChainId(hre);
+
+  const deploy = await hre.deployments.deploy('OneInchAggregatorSwapper', {
+    contract: 'contracts/swappers/OneInchAggregatorSwapper.sol:OneInchAggregatorSwapper',
+    from: deployer,
+    args: [governor, tradeFactory.address, AGGREGATION_ROUTER_V3[chainId]],
+    log: true,
+  });
+
+  await hre.deployments.execute('SwapperRegistry', { from: governor, gasLimit: 200000 }, 'addSwapper', 'one-inch-aggregator', deploy.address);
+
+  if (await shouldVerifyContract(hre, 'OneInchAggregatorSwapper')) {
+    await hre.run('verify:verify', {
+      address: deploy.address,
+      constructorArguments: [governor, tradeFactory.address, AGGREGATION_ROUTER_V3[chainId]],
+    });
+  }
+};
+deployFunction.dependencies = ['SwapperRegistry', 'TradeFactory'];
+deployFunction.tags = ['OneInchAggregatorSwapper'];
+export default deployFunction;
